3eb6224e9e48e38ca179be7910cde8339b963a5d
mavas <10604882+mavas@users.noreply.github.com>
2022-07-24 00:12:04 -0700

    New case

---
+++ code/new_year_chaos.py
@@ -0,0 +1,12 @@
+"""
+https://www.hackerrank.com/challenges/one-week-preparation-kit-new-year-chaos/problem
+"""
+
+
+def minimumBribes(a):
+    o = 0
+    return o
+ 
+
+if __name__ == '__main__':
+    assert minimumBribes([1, 2, 3, 5, 4, 6, 7, 8]) == "Too chaotic"

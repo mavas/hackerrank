ec6dbbfb8b4177da452b330b463e56fbf4b071b4
mavas <10604882+mavas@users.noreply.github.com>
2023-03-23 15:36:44 -0700

    updates

---
+++ code/greatest_possible_product.py
@@ -0,0 +1,21 @@
+"""
+https://apply.workable.com/screenly/j/CD139FE47C/apply/
+
+Finally it would greatly help our final evaluation process if you could provide
+me with a slightly more complex example of your programming. Consider the
+following problem: Given an array of positive integers, return the greatest
+possible product among any two integers in the array. I.e. find a[i] * a[j]
+such that i != j and a[i] * a[j] is the greatest possible. What would your
+solution be in your preferred programming language? The solution is only a few
+lines of code, so it shouldn't take too long. Please send me your completed
+source code with a description of why you choose your particular
+implementation.
+"""
+
+
+def greatest_possible_product(a):
+    return 54
+ 
+
+if __name__ == '__main__':
+    assert greatest_possible_product([2, 540, 21, 24, 9]) == 4

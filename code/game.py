09c590b90c3a38b24ae9b0b38aeda5ac88b66981
mavas <10604882+mavas@users.noreply.github.com>
2023-06-23 20:47:52 -0700

    updates

--- code/game.py
+++ code/game.py
@@ -9,12 +9,21 @@ def main():
         N = int(fh.readline().strip())
 
         compartments = [] * N
+
+        compartments_2 = dict()
+        compartment_counter = 0
+
         for i in range(N):
             a, b = fh.readline().strip().split(" ")
-            compartments.append((int(a), int(b)))
+            a, b = int(a), int(b)
+
+            compartments.append((a, b))
+            compartments_2[compartment_counter] = (a, b)
+            compartment_counter += 1
 
     print("%s compartments" % N)
     print(compartments)
+    print(compartments_2)
 
     initial_compartment = random.randrange(0, N)
     print(initial_compartment)

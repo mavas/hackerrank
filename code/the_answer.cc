04dd518189e8c218d674d3023b427074713c12e3
mavas <10604882+mavas@users.noreply.github.com>
2023-06-30 15:18:17 -0700

    Add C++ version of code.

---
+++ code/the_answer.cc
@@ -0,0 +1,23 @@
+/**
+ * https://codeforces.com/gym/104022/problem/I
+ */
+
+
+#include <stdio.h>
+
+
+int main(int argc, char* argv[])
+{
+    int N;
+    scanf("%d\n", &N);
+    printf("%d\n", N);
+
+    int compartments[N];
+
+    for (int count=0; count < N; count+=1)
+    {
+
+    }
+
+    return 0;
+}

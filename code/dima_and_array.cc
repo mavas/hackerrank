b30cac99652334425e705f4d6960517f346cf23b
mavas <10604882+mavas@users.noreply.github.com>
2023-07-08 04:15:46 -0700

    This compiles and runs

---
+++ code/dima_and_array.cc
@@ -0,0 +1,32 @@
+/**
+ * https://codeforces.com/gym/102069/problem/D
+ */
+
+
+#include <stdio.h>
+
+
+int main(int argc, char* argv[])
+{
+    int N, Q;
+    scanf("%d %d\n", &N, &Q);
+    printf("%d, %d\n", N, Q);
+    return 0;
+    int count = 0;
+
+    int x1, y1, x2, y2;
+
+    //while (count < 600)
+    while (count < 1)
+    {
+        char c;
+
+        printf("Q %d %d %d %d\n", x1, y1, x2, y2);
+        scanf("%c\n", &c);
+        //printf("\t%c\n", c);
+
+        count += 1;
+    }
+
+    return 0;
+}

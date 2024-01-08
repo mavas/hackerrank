b2be18fd6edeab05bf85549c68251b6c15160aa8
mavas <10604882+mavas@users.noreply.github.com>
2024-01-07 21:23:40 -0800

    updates

--- code/equal_stacks.py
+++ code/equal_stacks.py
@@ -10,39 +10,54 @@ def equal_heights(a, b, c):
 
 
 def equal_stacks(a, b, c):
+    f1 = sum(a)
+    f2 = sum(b)
+    f3 = sum(c)
 
-    while not equal_heights(a, b, c):
-        f1 = sum(a)
-        f2 = sum(b)
-        f3 = sum(c)
+    while not f1 == f2 == f3:
 
         if f1 != f2 and f2 != f3 and f1 != f3:
             if f1 > f2 and f2 > f3:
                 a = a[1:]
+                f1 = sum(a)
             elif f2 > f1 and f1 > f3:
                 b = b[1:]
+                f2 = sum(b)
             else:
                 c = c[1:]
+                f3 = sum(c)
+
         elif f1 == f2:
             if f2 > f3:
                 a = a[1:]
+                f1 = sum(a)
                 b = b[1:]
+                f2 = sum(b)
             else:
                 c = c[1:]
+                f3 = sum(c)
+
         elif f1 == f3:
             if f2 < f3:
                 a = a[1:]
+                f1 = sum(a)
                 c = c[1:]
+                f3 = sum(c)
             else:
                 b = b[1:]
+                f2 = sum(b)
+
         elif f2 == f3:
             if f1 < f2:
                 b = b[1:]
+                f2 = sum(b)
                 c = c[1:]
+                f3 = sum(c)
             else:
                 a = a[1:]
+                f1 = sum(a)
 
-    return sum(a)
+    return f1
 
 
 if __name__ == '__main__':
